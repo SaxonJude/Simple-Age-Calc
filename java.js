a921da3a6a08278b6cb1db8580aedf1054a592c4
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-09-02 13:39:03 +0100

    Stopped page refreshing when 'Enter'🖋️

--- java.js
+++ java.js
@@ -2,15 +2,20 @@
 var button, text;
 button = document.querySelector('.button');
 text = document.querySelector('.output_txt');
+title = document.querySelector('.title');
 
 // Event listeners
 document.querySelector('.age').addEventListener('keypress', (e) => {
   if(e.key === 'Enter'){
+    e.preventDefault();
     changeText();
   }
 });
 
 button.addEventListener('click', changeText);
+title.addEventListener('click', function() {
+  name.value = '0'
+})
 
 // Calculate Age Function
 function changeText() {
